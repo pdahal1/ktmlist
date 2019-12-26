ac2473e503c82925352c2d37f1fc0341ed219434
pdahal1 <dahalpar@gmail.com>
2019-12-25 23:02:06 -0500

    kk

--- app.js
+++ app.js
@@ -6,24 +6,25 @@ const { mongoose } = require('./config/database.js');
 const path = require ("path"); 
 //var expenseController = require('./controllers/expenseController.js');
 var registrationController = require('./routes/registrationController.js');
-var userController = require('./routes/userController.js');
-var entryController = require('./routes/entryController'); 
-var listingController = require('./routes/listingController');
+// var userController = require('./routes/userController.js');
+// var entryController = require('./routes/entryController'); 
+// var listingController = require('./routes/listingController');
 
 //const port = process.env.PORT || 8080; 
- const port = process.env.PORT||8081; 
+ const port = process.env.PORT || 8080; 
 
 var app = express(); 
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
-app.listen ( port , () => console.log('app fired up at port 3000 ok')); 
+app.use('/', express.static(path.join(__dirname, 'public')));
+ 
 //app.listen ( 3000 , () => console.log('app fired up at port 3000 ok')); 
 //app.use('/expenses', expenseController);
 // app.use('/users', userController);
 app.use('/register', registrationController ); 
 // app.use('/entries', entryController); 
 // app.use('/listings', listingController);
- app.use('/', express.static(path.join(__dirname, 'public'))); 
+  
 // Index Route
 app.get('/', (req, res) => {
     res.send('invaild endpoint');
@@ -32,3 +33,5 @@ app.get('/', (req, res) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
+
+  app.listen ( port , () => console.log('app fired up on ' + port));
