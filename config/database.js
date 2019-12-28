f5364f18b1d7e7068a0039da04ccd916d92675c5
pdahal1 <dahalpar@gmail.com>
2019-12-28 01:59:45 -0500

    git check

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://param:param5@ds027385.mlab.com:27385/heroku_c60wcp0z', (err) => { 
 // mongoose.connect('mongodb+srv://param:param@cluster0-spkms.mongodb.net/registrations?retryWrites=true&w=majority', (err) => {
     if (!err) {
-        console.log('connected successfully');
+        console.log('connected successfullys');
     } else {
         console.log('cannot connect to the database' + JSON.stringify(err, undefined, 2));
     }
